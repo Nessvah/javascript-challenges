c2e6ae0dac13ace9726ea8a142bac83eb5105221
Nessvah <vanessamarquesns@outlook.pt>
2022-06-29 17:51:25 +0100

    fifth challenge

--- scripts.js
+++ scripts.js
@@ -71,3 +71,23 @@ function positiveSum(arr) {
   }
   return total; // return total
 }
+
+////////////////////////////////////////////////////////
+/// Fourth challenge
+
+/* Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest' */
+
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
+
+/// SECOND OPTION
+/* function greet (name, owner) {
+  return `Hello ${name==owner?'boss':'guest'}`
+} */
